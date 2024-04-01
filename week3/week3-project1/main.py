32b4b0e49b9c8f07348d2a9b7333d2e52f58db2d
Abba00 <aaliconcern@gmail.com>
2024-04-01 02:08:05 +0100

    initial project commit

---
+++ week3/week3-project1/main.py
@@ -0,0 +1,160 @@
+# Import libraries
+import cv2 
+import os
+import time
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Input validation
+def getInput(prompt, t):
+    while True:
+        v = input(f'{prompt}: ')
+        try:
+            return t(v)
+        except ValueError:
+            print('Invalid input')
+
+# Image Directories
+dir_path = f"{os.getcwd()}/art"
+
+# Authenticate Users
+def authenticate(username_input, age_input):
+    """Authenticate users through username and password"""
+
+    suffix = "@pau.edu.ng"
+
+    if username_input.endswith(suffix):
+        if age_input > 18:
+            print(f"\nAccess Granted. Welcome {username_input[:-len(suffix)]}.")
+            return True
+        else:
+            print("You must be above 18 to access the art collections.")
+    else:
+        print("You must have a valid PAU email address ending with '@pau.edu.ng'.")
+
+# Fetch image database
+def get_images(choice):
+    """Fetch profile picture from database/directory"""
+
+    match choice:
+        case 0:
+            image_path = f"{dir_path}/contemporary/"
+
+        case 1:
+            image_path = f"{dir_path}/modern/"
+
+        case 2:
+            image_path = f"{dir_path}/traditional/"
+        
+        case _:
+            print("Invalid Option. Please try again.")
+            return False
+    
+    if os.path.exists(image_path):
+        return image_path, os.listdir(image_path)
+    else:
+        print("Art collection database not found. Please report issue to administrator.")
+        return None, None
+
+
+def enhance(choice, image_path):
+    """Enhancement Technique\n
+       Choice -> for technique chosen\n
+       Image -> image being fed into function"""
+
+    img = cv2.imread(image_path, 0)                
+    if choice == 0:
+        # Blurs
+        pass
+
+    else:
+        #Plot the original image
+        plt.subplot(1, 2, 1)    #row, column, column position
+        plt.title("Original")
+        plt.imshow(img)        
+        rows, cols = img.shape
+
+        match choice:
+            # Translate Image
+            case 1:
+                M = np.float32([[1, 0, 100], [0, 1, 50]])
+                result = cv2.warpAffine(img, M, (cols, rows))
+
+                #Plot the translated image
+                plt.subplot(1, 2, 2)
+                plt.title("Translated Image")
+
+            # Reflect Image
+            case 2: 
+                M = np.float32([[1, 0, 0], [0, -1, rows], [0, 0, 1]])
+                result = cv2.warpPerspective(img, M,(int(cols), int(rows)))
+
+                #Plot the reflected image
+                plt.subplot(1, 2, 2)
+                plt.title("Reflected Image")
+
+            # Rotate Image
+            case 3:
+                result = cv2.warpAffine(img, cv2.getRotationMatrix2D((cols/2, rows/2), 30, 0.6), (cols, rows))
+
+                #Plot the reflected image
+                plt.subplot(1, 2, 2)
+                plt.title("Rotated Image")
+
+            # Cropped Image
+            case 4:
+                result = img[450:700, 200:500]
+
+                #Plot the reflected image
+                plt.subplot(1, 2, 2)
+                plt.title("Cropped Image") 
+
+            # X-Axis Shearing
+            case 5:
+                M = np.float32([[1, 0.5, 0], [0, 1, 0], [0, 0, 1]])
+                result = cv2.warpPerspective(img, M, (int(cols*1.5),
+                                                        int(cols*1.5)))
+
+                #Plot the translated image
+                plt.subplot(1, 2, 2)
+                plt.title("Sheared Image X-Axis")
+
+            # Y-Axis Shearing
+            case 6:
+                M = np.float32([[1, 0, 0], [0.5, 1, 0], [0, 0, 1]])
+                result = cv2.warpPerspective(img, M, (int(cols*1.5),
+                                                        int(cols*1.5)))
+
+                #Plot the translated image
+                plt.subplot(1, 2, 2)
+                plt.title("Sheared Image Y-Axis")
+                                
+            case _:
+                print("No technique selected.")
+
+        plt.imshow(result)
+    plt.show()
+
+
+def main():
+    print("Welcone to the Yemisi Shyllon Museum of Art Image database. \nPlease enter your valid PAU email address and age to access the art collections.")
+    email = input("Email: ")
+    age = getInput("Age", int)
+
+    if authenticate(email, age):
+            redo = True
+            while redo:
+                choice = getInput("Input the number of the art category would you like to enhance:\n(1) Contemporary Art \n(2) Modern Art\n(3) Traditional Art\n\nChoice", int)
+                image_path, images = get_images(choice)
+                if images != None:
+                    print("\n")
+                    [print(f"({index}) {image}") for index, image in enumerate(images)]
+                    image = getInput(f"\nChoose the number of the image you wish to transform: ", int)
+                    transformation = getInput("\nChoose the number of the transformation you wish to perform on the image:", int)
+                    enhance(transformation, image_path+images[image])
+                    
+
+                
+if __name__ == "__main__":
+    main()
